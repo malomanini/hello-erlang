0347665c5b379cc1dd9dbb9131263386b8933d0b
root <root@valeria.bad>
2016-07-21 11:39:10 +0200

    ranch prot

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Launch with the script start.sh
 then use the command : application:ensure_all_started(hello_world).
 
 You can check the node name using any browser at http://localhost:8080/
+
+#Attach a node
+pool:attach(nodeName).
--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo -e "'`hostname --long`'.\n" > ~/.hosts.erlang
+
+if [ $# -eq 0 ]
+	then
+		NAME="nodeTheFirst"
+	else
+		NAME=$1
+fi
+
 exec erl -pa `pwd`/ebin -pa `pwd`/deps/*/ebin \
-     -sname nodeTheFirst
+     -sname $NAME
